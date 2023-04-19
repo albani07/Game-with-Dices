ecebcfe000fe1eb2610316ffe19a6271c77eb2c4
albani07 <alban07halaj@gmail.com>
2023-04-19 14:40:33 +0200

    hiding dice pic and making zero scores

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('.player');
+document.querySelector('.player--0');
+document.querySelector('.player--active');
+document.querySelector('.name');
+document.getElementById('name--0');
+document.querySelector('.score');
+const scoreEl0 = document.getElementById('score--0');
+document.querySelector('.current');
+document.querySelector('.current-label');
+document.querySelector('.current-score');
+document.querySelector('.player');
+document.querySelector('.player--1');
+document.getElementById('name--1');
+const scoreEl1 = document.getElementById('score--1');
+document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+document.querySelector('.btn');
+document.querySelector('.btn--new');
+document.querySelector('.btn--roll');
+document.querySelector('.btn--hold');
+
+scoreEl0.textContent = 0;
+scoreEl1.textContent = 0;
+diceEl.classList.add('hidden')
